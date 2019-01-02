b8960805cb4240f8eec6e5243e14f54d8bfa4b3c
Kyle Kilbride <kyle.kilbride@gmail.com>
2019-01-02 16:06:16 -0500

    code for angie to pull

---
+++ variables/variables.js
@@ -0,0 +1,44 @@
+// created variables
+var one = 1
+let two = 2
+const three = 3
+
+// printed the variables out
+console.log('printing original vars')
+console.log(one)
+console.log(two)
+console.log(three)
+
+// edit the variables
+// CHANGE THE 'ONE' VARIABLE
+one = one + 5
+console.log('adding five to one variable')
+console.log(one)
+
+// strings
+let stringVariable = 'this is a string'
+
+// console logging strings
+console.log(stringVariable)
+
+// console loggin multiple strings
+console.log('this is' + ' the second half!')
+
+// you can build a custom string from multiple variable like so:
+let firstPart = 'Kyle is the'
+let secondPart = 'coolest teacher ever!'
+console.log(firstPart + ' ' + secondPart)
+
+/**
+ * TESTING TIME!
+ */
+
+// create a constant variable called Kyle and set it to the string 'Kyle'
+
+// create a regular variable called Angie and set it to the string 'Angie'
+
+// using the above example, console log this string: 'Kyle loves Angie'
+
+// just for fun change the variable Angie to the string 'Guitar'
+
+// now console log the string 'Kyle loves Guitar'
